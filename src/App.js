1666f64459010f14684c9c02fe41319cd1530319
Adam Wong <adamwthedev@gmail.com>
2021-05-13 22:03:45 -0700

    began working on Login component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import { Route, withRouter } from 'react-router-dom'
+import Login from './components/login'
 import Styled from 'styled-components'
 
-const AppDiv = Styled.div``
+const AppDiv = Styled.div`
+  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif
+`
+
+const renderRoutes = () => {
+  return (
+    <>
+      <Route exact path="/" component={Login}/>
+    </>
+  )
+}
 
 const App = () => {
   return (
     <AppDiv>
-      <h1>App Component</h1>
+      {renderRoutes()}
     </AppDiv>
   )
 }
--- src/Styles/globalStyles.js
+++ src/Styles/globalStyles.js
@@ -1,9 +1,14 @@
 const globalStyles = {
     "heading": "#262626",
     "body": "#fafafa",
+    "input": "#fafafa",
     "border": "#dbdbdb",
+    "login": "#0095f6",
+    "signUp": "#0095f6",
+    "or": "#8e8e8e",
     "facebook": "#385185",
     "forgotPassword": "#00376b",
-    "signUp": "#0095F6",
     "footer": "#8e8e8e"
-}
+};
+
+export default globalStyles;
