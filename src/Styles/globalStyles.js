1e9730f4f66a1e6c4ff9b7397a45a1fdffcba260
Adam Wong <adamwthedev@gmail.com>
2021-05-13 17:36:30 -0700

    created components and styles folder

---
+++ src/Styles/globalStyles.js
@@ -0,0 +1,9 @@
+const globalStyles = {
+    "heading": "#262626",
+    "body": "#fafafa",
+    "border": "#dbdbdb",
+    "facebook": "#385185",
+    "forgotPassword": "#00376b",
+    "signUp": "#0095F6",
+    "footer": "#8e8e8e"
+}
