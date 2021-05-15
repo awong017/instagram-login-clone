513fa8f72e327cf5c13dd5ce6fc4ad6da23fb9da
Adam Wong <adamwthedev@gmail.com>
2021-05-14 23:22:41 -0700

    created Home component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Route, withRouter } from 'react-router-dom'
 import Context from './context'
 import Landing from './components/landing'
+import Home from './components/home'
 import SignUpPage from './components/signUpPage'
 import Footer from './components/footer'
 import Styled from 'styled-components'
@@ -14,18 +15,19 @@ const renderRoutes = () => {
   return (
     <>
       <Route exact path="/" component={Landing}/>
+      <Route exact path="/home" component={Home}/>
       <Route path="/signUp" component={SignUpPage} />
       <Route path="/" component={Footer} />
     </>
   )
 }
 
-const App = () => {
+const App = (props) => {
   const [ users, setUsers ] = useState(
     [
       {
         id: 1,
-        userName: "awong017",
+        username: "awong017",
         email: "awong017@ucr.edu",
         phone: 7143081649,
         password: "password"
@@ -33,8 +35,55 @@ const App = () => {
     ]
   )
 
+  const [ loginError, setLoginError ] = useState({usernameError: "", passwordError:""})
+  
+  const [ currentUser, setCurrentUser ] = useState()
+
+  // Method for logging in
+
+  const handleLogin = (e, name, password) => {
+    e.preventDefault()
+
+    if (
+      users.some(user => user.username === name) === false &&
+      users.some(user => user.email === name) === false &&
+      users.some(user => user.password === name) === false
+    ) {
+      setLoginError(
+        {
+          usernameError: "The username you entered doesn't belong to an account. Please check your username and try again.",
+          passwordError: ""
+        }
+      )
+    }
+    else if (!password) {
+      setLoginError(
+        {
+          usernameError: "",
+          passwordError: "Sorry, your password was incorrect. Please double-check your password."
+        }
+      )
+    }
+    else {
+      let userAccount = users.find(user => user.username === name)
+      setLoginError({usernameError: "", passwordError: ""})
+      setCurrentUser(
+        {
+          username: userAccount.username,
+          email: userAccount.email,
+          phone: userAccount.phone,
+          password: userAccount.password
+        }
+      )
+      props.history.push("/home")
+    }
+  }
+
   const contextValue = {
-    users: users
+    users: users,
+    loginError: loginError,
+    currentUser: currentUser,
+    handleLogin: handleLogin
   }
 
   return (
